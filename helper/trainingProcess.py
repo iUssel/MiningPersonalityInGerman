Exported features - bug fix glove feature calculation

helper/trainingProcess.py
@@ -392,6 +392,8 @@ class TrainingProcess:
         readPickleFiles=False,
         writeONNXModel=False,
         readONNXModel=False,
+        writeFeatureFile=False,
+        readFeatureFile=False,
     ):
         """
         TODO doGloVeModelTraining
@@ -411,6 +413,12 @@ class TrainingProcess:
         if readPickleFiles is True and readONNXModel is True:
             raise Exception(
                 "readPickleFiles and readONNXModel cannot be " +
+                "True at the same time."
+            )
+
+        if writeFeatureFile is True and readFeatureFile is True:
+            raise Exception(
+                "writeFeatureFile and readFeatureFile cannot be " +
                 "True at the same time."
             )
 
@@ -473,16 +481,40 @@ class TrainingProcess:
         else:
             print("\nStart of GloVe Model Training")
 
-            # path for GloVe vectors
-            file_path = Path(
-                'data/glove/glove.db'
-            )
-            # create feature pipeline
-            features = Features()
-            gloveFeaturePipeline = features.createGloVeFeaturePipeline(
-                glovePath=file_path,
-                dataBaseMode=True
-            )
+            if readFeatureFile is True:
+                print("reading featureFile")
+                print("\nImporting calculated features")
+                # path for saved features
+                file_directory_string = (
+                    'data/08gloveFeatures'
+                )
+                # concatenate file path
+                file_path = Path(
+                    file_directory_string +
+                    ".npy"
+                )
+                # call numpy load function
+                calc_features = np.load(
+                    file=file_path,
+                    allow_pickle=False
+                )
+                print("Feature shape: " + str(calc_features.shape))
+                gloveFeaturePipeline = None
+            else:
+                # set to None
+                calc_features = None
+                # path for GloVe vectors
+                file_path = Path(
+                    #'data/glove/glove.db'
+                    'data/glove/glove_vectors.txt'
+                    #'data/glove/first_100000.txt'
+                )
+                # create feature pipeline
+                features = Features()
+                gloveFeaturePipeline = features.createGloVeFeaturePipeline(
+                    glovePath=file_path,
+                    dataBaseMode=False # TODO make flexible
+                )
 
             # create list of models with parameters
             # this list will be used for model selection
@@ -497,6 +529,8 @@ class TrainingProcess:
                 modelObjList=modelList,
                 featurePipeline=gloveFeaturePipeline,
                 profileColTraining=profileCol,
+                saveFeatures=writeFeatureFile,
+                precalculatedFeatures=calc_features,
             )
 
             # fully train model in different method of modelTraining
@@ -505,10 +539,32 @@ class TrainingProcess:
                 modelCollection=globalBestGloVeModels,
                 featurePipeline=gloveFeaturePipeline,
                 profileColTraining=profileCol,
+                saveFeatures=False,
+                precalculatedFeatures=calc_features,
             )
 
             # TODO word coverage analysis
-            print(features.coverageStatistics)
+            #print(features.coverageStatistics)
+
+            # write feature file
+            if writeFeatureFile is True:
+                print("\nExporting calculated features")
+                calc_features = modelTraining.features
+                # path for saving features
+                file_directory_string = (
+                    'data/08gloveFeatures'
+                )
+                # concatenate file path
+                file_path = Path(
+                    file_directory_string +
+                    ".npy"
+                )
+                # call numpy save function
+                np.save(
+                    file=file_path,
+                    arr=calc_features,
+                    allow_pickle=False
+                )
 
             # only export models if specified
             if writePickleFiles is True:

miping/training/features.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from ..models.profile import Profile
 from ..interfaces.helper import Helper
 from ..interfaces.glove import GloVe
+from .noGloveValueError import NoGloveValueError
 
 
 class Features:
@@ -93,6 +94,9 @@ class Features:
         # convert to np array for mean,max,min functions
         vectorList = np.array(vectorList)
 
+        # correct structure from (1,x,300) to (x,300)
+        vectorList = vectorList[0]
+
         # for each dimension identify mean,max,min
         # and save in separate vector
         meanVector = vectorList.mean(axis=0)
@@ -160,27 +164,36 @@ class Features:
             # so if words exist n times in text, they will be n times in list
             in_glove = [word for word in tokens if word not in not_in_glove]
 
-            # lookup glove vectors
-            # should return duplicates!
-            glove_values = self.glove.getGloVeByWordList(
-                wordList=in_glove
-            )
-            converted_vals = np.array(glove_values)
-            # add vectors to list of this profile's vectors
-            profile_vectors.append(converted_vals)
+            if len(in_glove) == 0:
+                # es konnte kein wort in glove gefunden werden
+                # raise Exception
+                eString = (
+                    "Could not find any glove values for given words"
+                )
+                raise NoGloveValueError(eString)
+            else:
+                # mind. ein Wort wurde gefunden
+                # lookup glove vectors
+                # should return duplicates!
+                glove_values = self.glove.getGloVeByWordList(
+                    wordList=in_glove
+                )
+                converted_vals = np.array(glove_values)
+                # add vectors to list of this profile's vectors
+                profile_vectors.append(converted_vals)
 
-            # fill coverage statistics as share of tokens (=words)
-            # that exist in glove in comparison to total tokens
-            profile_coverage = len(profile_vectors) / len(tokens)
-            # add to global list
-            coverageStatistics.append(profile_coverage)
+                # fill coverage statistics as share of tokens (=words)
+                # that exist in glove in comparison to total tokens
+                profile_coverage = len(profile_vectors) / len(tokens)
+                # add to global list
+                coverageStatistics.append(profile_coverage)
 
-            # after all vectors for this profile are retrieved
-            # condense with maximum, minimum, average in 900 dim vector
-            final_vector = self._condenseGloVeVectors(profile_vectors)
+                # after all vectors for this profile are retrieved
+                # condense with maximum, minimum, average in 900 dim vector
+                final_vector = self._condenseGloVeVectors(profile_vectors)
 
-            # add 900 dim to output list
-            outputList.append(final_vector)
+                # add 900 dim to output list
+                outputList.append(final_vector)
 
             # Update Progress Bar
             helper.printProgressBar(