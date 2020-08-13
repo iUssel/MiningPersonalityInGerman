c1d3cd2d762416035d4b3c2bdd03f7612c126d3d
Henning Usselmann <henning.usselmann@gmail.com>
2020-08-13 21:28:04 +0200

    v8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="miping",
-    version="0.0.7",
+    version="0.0.8",
     author="Henning Usselmann",
     author_email="miping@uber.space",
     description=(
